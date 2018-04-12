05d682bff70295ad59c9c37fe1b19918a9a1f336
jianyong <jianyong@mycapital.net>
2018-04-12 19:34:31 +0800

    Update readme

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
 # Overview
 
+[官方可集成版本](https://benweet.github.io/stackedit.js/)
+
 Vue-stackedit 是基于 [stackedit](https://stackedit.io) 开发的一款在线markdown编辑器。
 
-目前市面上已经有很多开源的的在线markdown编辑器了，经过多方调研后发现功能比较齐全且强大的有stackedit和作业部落的mdeditor，但是作业部落的产品并未开源，因为决定采用stackedit。
+目前市面上已经有很多开源的的在线markdown编辑器了，经过多方调研后发现功能比较齐全且强大的有stackedit和作业部落的mdeditor，但是作业部落的产品并未开源，因此决定采用stackedit。
 
 stackedit是一个完整的网站产品，但是很多情况下，我们只需要其中的编辑器功能，并且希望能方便地集成到自己的产品中，因此决定将其中的编辑器部分剥离出来，单独做成一个库，方便直接在自己的产品中调用。
 
-编辑出来的文件基本可用，但是源文件中的store部分其实很多都不需要，而直接删掉又会报错，其中关联有点复杂，因此直接保留下来，以后有时间再慢慢梳理。当然强烈欢迎志愿者做贡献。
+编辑出来的文件基本可用，但是源文件中的store部分其实很多都不需要，而直接删掉又会报错，其中关联有点复杂，因此直接保留下来，以后有时间再慢慢梳理。强烈欢迎志愿者做贡献。
 
 stackedit是基于Vue.js框架开发的，本项目沿用Vue.js框架，使用vue-cli工具开发。
 
-Demo: [Demo](https://www.yikeyong.com/spa/vue-stackedit.html)
+Demo: [Demo](http://112.74.83.47:8080/)
 
 # Usage
 
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,47 +1,4 @@
-export const NETWORKS = {
-  '1': 'Main Net',
-  '2': 'Deprecated Morden test network',
-  '3': 'Ropsten test network',
-  '4': 'Rinkeby test network',
-  '42': 'Kovan test network',
-  '4447': 'Truffle Develop Network',
-  '5777': 'Ganache Blockchain'
-}
-
 export const TEXT = {
-  cn: {
-    navbar: {
-      home: '首页'
-    },
-    home: {
-      connectTo: '连接网络',
-      publish: '发布信号',
-      currentCounts: '当前信号总数',
-      signals: '信号列表',
-      currentPrice: '最低竞拍价',
-      publisher: '发布者',
-      publishTime: '发布时间',
-      bid: '竞价'
-    }
-  },
-  en: {
-    navbar: {
-      home: 'Home'
-    },
-    home: {
-      connectTo: 'Connect to',
-      publish: 'Publish Your Signal',
-      currentCounts: 'Current signal counts',
-      signals: 'Signals',
-      currentPrice: 'Current price',
-      publisher: 'Publisher',
-      publishTime: 'Publish time',
-      bid: 'Bid'
-    }
-  }
+  cn: {},
+  en: {}
 }
-
-export const SIGNAL_STATUS = ['竞价中', '竞价结束', '信号已过期', '已取消']
-export const EQ_SIGNAL_STATUS = ['销售中', '信号已过期']
-export const BID_STATUS = ['竞价中', '竞价失败', '竞价成功', '竞价取消']
-
